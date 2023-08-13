65c31ef121acfd1c61a79010f18b255a0bd92755
Pyother <piotr.dominik.sobol@gmail.com>
2023-08-13 21:30:54 +0200

    Initial

--- app/consumers.py
+++ app/consumers.py
@@ -1,36 +1,15 @@
 import json
 import time
-import paho.mqtt.client as mqtt
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import SyncConsumer
 
-class MQTTConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
-        await self.send(text_data="Backend -> Connection established")
-        self.mqtt_client = mqtt.Client()
-        self.mqtt_client.on_connect = self.on_connect
-        self.mqtt_client.on_message = self.on_message
-        self.mqtt_client.connect("localhost", 1883, 60)
-        self.mqtt_client.loop_start()
-        print("Backend -> LOOP STARTED")
+class MqttConsumer(SyncConsumer):
+    def mqtt_sub(self, event):
+        topic = event['text']['topic']
+        payload = event['text']['payload']
+        # do something with topic and payload
+        print('{} - {}'.format(topic, payload))
 
-    async def receive(self, text_data=None, bytes_data=None):
-        await self.send(text_data="New message received!")
-
-    def on_connect(self, client, userdata, flags, rc):
-        client.subscribe("measurements")
-        print("Backend -> ✓ Client connected")
-
-    def on_message(self, client, userdata, msg):
-        message = msg.payload.decode("utf-8")
-        self.send(text_data=json.dumps({
-            "type": "mqtt_message",
-            "message": message
-        }))
-        print("Backend -> Received message: " + message)
-
-    async def disconnect(self, close_code):
-        self.mqtt_client.loop_stop()
-        print("Backend -> LOOP STOPPED")
+    def mqtt_pub(self, event):
+        pass
 
     
--- app/routing.py
+++ app/routing.py
@@ -1,9 +1,13 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path, re_path
-from . import consumers
+from django.core.asgi import get_asgi_application
+from .consumers import MqttConsumer
+from channels.routing import ProtocolTypeRouter, ChannelNameRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 application = ProtocolTypeRouter({
-    "websocket": URLRouter([
-        path("ws/dashboard/", consumers.MQTTConsumer.as_asgi()),
-    ]),
+    "http": get_asgi_application(),
+    "channel": ChannelNameRouter(
+        {
+            "mqtt.pub": MqttConsumer.as_asgi()
+        }
+    )
 })
